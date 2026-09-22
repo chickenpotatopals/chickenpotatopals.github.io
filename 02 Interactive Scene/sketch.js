53911f801d743f4e1e5f98e68a999182fed6b659
chickenpotatopals <6100673@spsd.sk.ca>
2026-09-22 14:08:33 -0600

    Interactive Scene Day 2
    added ronald mcdonald. need to add more interactiveness and also some more shape types into the scene

--- 02 Interactive Scene/sketch.js
+++ 02 Interactive Scene/sketch.js
@@ -11,6 +11,7 @@ async function setup() {
 function draw() {
   background(162, 207, 255);
   noStroke();
+  // draw the mcdonalds
   fill(94, 115, 138);
   rect(0, windowHeight * 0.8, windowWidth, windowHeight * 0.2);
   fill('white');
@@ -30,4 +31,37 @@ function draw() {
   fill("black");
   rect(windowWidth * 0.15, windowHeight * 0.58, windowWidth * 0.15, windowHeight * 0.15);
   rect(windowWidth * 0.35, windowHeight * 0.58, windowWidth * 0.1, windowHeight * 0.15);
+  // draw RONALD MCDONALD
+  stroke("black");
+  // limb base and feet
+  fill(217, 0, 0);
+  rect(mouseX, windowHeight * 0.68, 60, 20);
+  rect(mouseX - 60, windowHeight * 0.68, 60, 20);
+  rect(mouseX + 5, windowHeight * 0.72, 20, 60);
+  rect(mouseX - 25, windowHeight * 0.72, 20, 60);
+  circle(mouseX + 15, windowHeight * 0.80, 30);
+  circle(mouseX - 15, windowHeight * 0.80, 30);
+  // forearms, shins
+  fill(255, 255, 225);
+  rect(mouseX + 60, windowHeight * 0.68, 40, 20);
+  rect(mouseX - 100, windowHeight * 0.68, 40, 20);
+  rect(mouseX + 5, windowHeight * 0.76, 20, 40);
+  rect(mouseX - 25, windowHeight * 0.76, 20, 40);
+  // torso and hands
+  fill("yellow");
+  ellipse(mouseX, windowHeight * 0.71, 60, 100);
+  circle(mouseX + 100, windowHeight * 0.69, 25);
+  circle(mouseX - 100, windowHeight * 0.69, 25);
+  // head
+  fill(255, 255, 225);
+  circle(mouseX, windowHeight * 0.66, 50);
+  // red afro of doom
+  fill(217, 0, 0);
+  arc(mouseX, windowHeight * 0.65, 55, 50, 3, 6.5);
+  // seductive face
+  fill("black");
+  circle(mouseX + 8, windowHeight * 0.66, 5);
+  circle(mouseX - 8, windowHeight * 0.66, 5);
+  arc(mouseX, windowHeight * 0.67, 10, 5, 0, 3.5);
+
 }
